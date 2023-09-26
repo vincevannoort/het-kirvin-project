3736360adb3ae4470e11fd996305f81b4624a9cf
Vince van Noort <vincevannoort@gmail.com>
2023-09-26 07:17:03 +0200

    feat(data): basic analysis steps

--- kirvin/main.py
+++ kirvin/main.py
@@ -1,2 +1,22 @@
-if __name__ == "__main__":
-    print("test")
+import polars as pl
+
+from kirvin.load import load
+
+data_raw = load()
+
+data = pl.DataFrame(data_raw)
+
+print(
+    data.select(
+        # text columns
+        pl.col("ID"),
+        pl.col("KenmerkenBaan"),
+        pl.col("Perioden"),
+        pl.col("BedrijfstakkenBranchesSBI2008"),
+        # convert all other columns to floats
+        pl.all()
+        .exclude(["ID", "KenmerkenBaan", "Perioden", "BedrijfstakkenBranchesSBI2008"])
+        .str.strip_chars()
+        .cast(pl.Int32),
+    )
+)
