257a77500efb8d7b8f7d9cb5cd96e7e8a289631f
kirsten-bulsink <kirstenmariekebulsink@gmail.com>
2023-09-29 14:59:08 +0200

    new load method
    different library and updated query

--- kirvin/load.py
+++ kirvin/load.py
@@ -1,14 +1,12 @@
-import json
-from urllib import request
+import requests
 
 
 def load():
-    base_url = "https://opendata.cbs.nl/ODataApi/odata/83451NED/UntypedDataSet"
-    url = f"{base_url}?$filter=((KenmerkenBaan+eq+%2710000++%27)+or+(KenmerkenBaan+eq+%27100460+%27))+and+((BedrijfstakkenBranchesSBI2008+eq+%27T001081%27)+or+(BedrijfstakkenBranchesSBI2008+eq+%27389100+%27)+or+(BedrijfstakkenBranchesSBI2008+eq+%27422400+%27))+and+((Perioden+eq+%272023MM01%27)+or+(Perioden+eq+%272023MM02%27)+or+(Perioden+eq+%272023MM03%27))"
+    base_url = "https://opendata.cbs.nl/ODataApi/odata/83451NED/TypedDataSet"
+    url = f"{base_url}?$filter=((KenmerkenBaan+eq+%2710000++%27)+or+(KenmerkenBaan+eq+%273000+++%27)+or+(KenmerkenBaan+eq+%274000+++%27))+and+((Perioden+eq+%272023MM01%27)+or+(Perioden+eq+%272023MM02%27)+or+(Perioden+eq+%272023MM03%27))"
 
-    with request.urlopen(url) as file:
-        data_json = json.load(file)
-
+    response = requests.get(url)
+    data_json = response.json()
     data = data_json["value"]
 
     return data
