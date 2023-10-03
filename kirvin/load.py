2e4f6820ab7ff565d2f32043511d74c1d07d2a65
Vince van Noort <vincevannoort@gmail.com>
2023-10-03 22:09:14 +0200

    fix(data): reference data relative from project root

--- kirvin/load.py
+++ kirvin/load.py
@@ -3,7 +3,7 @@ from polars import DataFrame, read_csv
 
 def load() -> DataFrame:
     data = read_csv(
-        source="../data/jaar.txt",
+        source="data/jaar.txt",
         skip_rows=31,
     )
     return data
