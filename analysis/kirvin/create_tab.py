52cee12bc38df3cbd63de12a4fa58c6e3ec44d44
kirsten-bulsink <kirstenmariekebulsink@gmail.com>
2023-10-18 21:46:17 +0200

    change to enum

--- analysis/kirvin/create_tab.py
+++ analysis/kirvin/create_tab.py
@@ -1,10 +1,12 @@
 from polars import DataFrame, col
 
+from kirvin.columns import Column
+
 
 def create_tab(data: DataFrame) -> DataFrame:
     tab = (
-        data.with_columns(col("temperature") / 10)
-        .group_by("date", maintain_order=True)
-        .agg(col("temperature").mean())
+        data.with_columns(col(Column.temperature) / 10)
+        .group_by(Column.date, maintain_order=True)
+        .agg(col(Column.temperature).mean())
     )
     return tab
