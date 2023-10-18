5a582a2170ecb493de6fd50c6f9d65ee1bfeb398
kirsten-bulsink <145478700+kirsten-bulsink@users.noreply.github.com>
2023-10-18 21:41:00 +0200

    Update analysis/kirvin/create_tab.py
    Co-authored-by: Vince van Noort <vincevannoort@gmail.com>

--- analysis/kirvin/create_tab.py
+++ analysis/kirvin/create_tab.py
@@ -1,7 +1,7 @@
 from polars import DataFrame, col
 
 
-def create_tab(data: DataFrame):
+def create_tab(data: DataFrame) -> DataFrame:
     tab = (
         data.with_columns(col("temperature") / 10)
         .group_by("date", maintain_order=True)
