3c61c2ba259fd3f6f085bdfd4838499d07d738ae
Vince van Noort <vincevannoort@gmail.com>
2023-11-09 23:24:26 +0100

    feat(analysis): add graph for sunshine per day

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import styles from './page.module.css'
 import TemperaturePerStation from '@/components/temperature-per-station'
+import SunshinePerStation from '@/components/sunshine-per-station'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <TemperaturePerStation />
+      <hr style={{ margin: 50 }} />
+      <SunshinePerStation />
     </div>
   )
 }
---
+++ dashboard/components/sunshine-per-station.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import sunshine_per_day from '@/data/sunshine_per_day.json'
+import Highcharts, { SeriesLineOptions, SeriesOptionsType } from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+
+export default function TemperaturePerStation() {
+
+
+    let series: Array<SeriesLineOptions> = [
+        // create line for average
+        {
+            type: "line",
+            opacity: 1,
+            dashStyle: "LongDash",
+            lineWidth: 1,
+            color: 'blue',
+            zIndex: 100,
+            name: 'Average (7d)',
+            data: sunshine_per_day.map(d => [d.date, d.sunshine_duration]),
+        },
+    ]
+
+
+
+
+    const options: Highcharts.Options = {
+        chart: {
+            height: 700,
+        },
+        title: {
+            text: 'Sunshine per day',
+        },
+        tooltip: {
+            shared: true
+        },
+        subtitle: {
+            text: 'Example',
+        },
+        yAxis: {
+            title: {
+                text: 'Sunshine duration in hours'
+            }
+        },
+        xAxis: {
+            type: 'datetime',
+            title: {
+                text: 'Day'
+            }
+        },
+        series: series
+    }
+
+
+    return (
+        <div>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
+        </div>
+    )
+}
