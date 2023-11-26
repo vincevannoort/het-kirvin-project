6278386e0988963811af2f6125339d9012c2ee63
Vince van Noort <vincevannoort@gmail.com>
2023-11-26 21:11:36 +0100

    feat(dashboard): add date picker component

---
+++ dashboard/components/date-range-picker.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import Datepicker, { DateValueType } from "react-tailwindcss-datepicker";
+
+import { create } from 'zustand'
+
+type DateRange = {
+    startDate: Date,
+    endDate: Date,
+}
+
+interface DateRangeState {
+    dateRange: DateRange
+    update: (value: DateRange) => void
+}
+
+export function timestamp_within_date_range(timestamp: number, dateRange: DateRange): boolean {
+    return timestamp >= dateRange.startDate.getTime() && timestamp <= dateRange.endDate.getTime()
+}
+
+export const useDateRangeStore = create<DateRangeState>()((set) => ({
+    dateRange: {
+        startDate: new Date(new Date().getFullYear(), 0, 1),
+        endDate: new Date(),
+    },
+    // input is type provided by react-tailwindcss-datepicker, 
+    // we convert it to our own type which cannot be null
+    update: (value: DateValueType) => {
+        // ensure we always have a valid date range
+        if (!value || !value.startDate || !value.endDate) {
+            return
+        }
+
+        set({
+            dateRange: {
+                startDate: new Date(value.startDate),
+                endDate: new Date(value.endDate),
+            }
+        })
+    },
+}))
+
+
+export function DateRangePicker() {
+    const { update, dateRange } = useDateRangeStore();
+
+    return (
+        <>
+            <div className="flex items-center py-4">
+                <p className="whitespace-nowrap">Date range:</p>
+                <Datepicker
+                    value={dateRange}
+                    onChange={update}
+                    showShortcuts={true}
+                    containerClassName={"relative text-gray-700"}
+                />
+            </div>
+        </>
+    );
+}
